3b78fcf24a4f5c77004b08cce97fd02e538e829c
Juansebast123 <166045022+Juansebast123@users.noreply.github.com>
2025-08-13 11:52:53 -0500

    Add files via upload

---
+++ Gramatica1/gramatica1p.py
@@ -0,0 +1,32 @@
+def funcion(cadena):
+    i = 0
+    j = len(cadena) - 1
+
+    while i < j:
+        if (cadena[i] not in ('0', '1')) or (cadena[j] not in ('0', '1')):
+            return False
+        if cadena[i] != cadena[j]:
+            return 0
+        i += 1
+        j -= 1
+
+    return len(cadena) > 0
+
+
+def main():
+    import sys
+
+    try:
+        with open(sys.argv[1], "r") as file:
+            for linea in file:
+                cadena = linea.strip()
+                if funcion(cadena):
+                    print(f"{cadena} = acepta")
+                else:
+                    print(f"{cadena} = NO acepta")
+    except FileNotFoundError:
+        print("Error al abrir el archivo")
+
+
+if __name__ == "__main__":
+    main()
